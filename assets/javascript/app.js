4f8156e5226b13f5ac8017785873ec01889e3a9f
Melinda Hardin <melindawad@gmail.com>
2018-02-12 21:25:12 -0600

    style

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//click start button. 
+
+//  goes to next screen displaying...
+
+//      TIMER at the top (for one qustion at a time).
+//      Question under timer. 
+//      4 options.
+//          options highlight on hover
+//          pointer on hover
+//          when wrong answer is clicked= "nope" and displays picture with correct answer and move to next question and timer restarts
+
+//  after all quesions answered
+//      timer stops
+//      displays correct, incorrect and unanswered questions.
+//      start over button (highlights on hover)
+
+//  start over clicked
+//      resets the game, NOT reloads the page. 
