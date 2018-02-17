5d3cf7dc4e3d3554d7bf0b924254348ab0e21cd7
Melinda Hardin <melindawad@gmail.com>
2018-02-16 20:22:44 -0600

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,36 @@
-
 $(document).ready(function(){
 
 
-
+    var timer = 10;
+    var totalQuestions= 10;
+    var totalCorrect= "";
 
     $(".questions").hide();
-$("#start-button").show();
+    $("#start-button").show();
+
+    $("#start-button").click(gameStart);
+
+        function gameStart(){
+            $(".questions").hide();
+            $("#start-button").hide();
+            
+            $("#q1").show();
+            setInterval(nextQuestion, 1000*10);
+
+        }
+  
+            function nextQuestion (){
+                $("#start-button").hide();
+                $(".questions").hide();
+                $("#q2").show();
+                
+                
+                
+            }
+
+
+
+
+
     
-$("#start-button").click(gameStart);
-
-
-
-
-function gameStart(){
-    $("#q1").show()
-}
-
-
-
-
-
-    
-})
+});
