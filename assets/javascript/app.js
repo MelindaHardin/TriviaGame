4dbd9b6a388aa1e0b75f19d16b898b64fe53b619
Melinda Hardin <melindawad@gmail.com>
2018-02-16 19:11:04 -0600

    button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,24 @@
 
+$(document).ready(function(){
+
+
+
+
+    $(".questions").hide();
+$("#start-button").show();
+    
+$("#start-button").click(gameStart);
+
+
+
+
+function gameStart(){
+    $("#q1").show()
+}
+
+
+
+
+
+    
+})
